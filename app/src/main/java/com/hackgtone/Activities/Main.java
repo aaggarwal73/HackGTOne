fe55affcc6550202433594f7d7f50650942eb741
Aman <aaggarwal73@gatech.edu>
2017-10-14 21:47:55 -0400

    moved around files and created sample data

--- app/src/main/java/com/hackgtone/Activities/Main.java
+++ app/src/main/java/com/hackgtone/Activities/Main.java
@@ -1,17 +1,25 @@
 package com.hackgtone;
 
+import com.hackgtone.model.*;
+
 /**
  * Created by Aman on 10/14/17.
  */
 
 class Main {
-    private static final Main ourInstance = new Main();
 
+    private static final Main ourInstance = new Main();
+    private static String currentTrip; //change to type Trip later
+    private static String chosenLeg; //change to type Itinerary later
+    private static Service services;
+    private static Trip trips;
+    private Main() {
+
+    }
     static Main getInstance() {
         return ourInstance;
     }
-    private static String currentTrip; //change to type Trip later
-    private static String chosenLeg; //change to type Itinerary later
+
 
 //    public static void changeScreen(String viewFile) {
 //        try {
@@ -33,6 +41,5 @@ class Main {
     public static String getCurrentTrip() {
         return currentTrip;
     }
-    private Main() {
-    }
+
 }
